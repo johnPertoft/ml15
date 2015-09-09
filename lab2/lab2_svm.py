1451ab6988579b80d5677118e32470a3a13bd2b7
John Pertoft <john@pertoft.se>
2015-09-09 15:09:33 +0200

    fixed indentation

--- lab2/lab2_svm.py
+++ lab2/lab2_svm.py
@@ -5,7 +5,7 @@ from cvxopt.base import matrix
 import numpy, pylab, random, math
 
 def linearKernel(x, y):
-   return x.dot(y) + 1;
+    return x.dot(y) + 1;
 
 def polynomialKernel(x, y, p):
     return numpy.power(x.transpose() * y + 1, p)
@@ -17,16 +17,16 @@ def sigmoidKernel(x, y, k, delta):
     return numpy.tanh(k * x.transpose() * y - delta)
 
 def kernel(x, y):
-	return linearKernel(x,y)
+    return linearKernel(x,y)
 
 def buildMatrix(data):
-	P = [0] * len(data)
-	for i in range(0, len(data)):
-		P[i] = [0] * len(data)
-		for j in range(0, len(data)):
-			P[i][j] = data[i][2] * data[j][2] * kernel(numpy.array(data[i][0:2]), numpy.array(data[i][0:2]))
+    P = [0] * len(data)
+        for i in range(0, len(data)):
+            P[i] = [0] * len(data)
+                for j in range(0, len(data)):
+                    P[i][j] = data[i][2] * data[j][2] * kernel(numpy.array(data[i][0:2]), numpy.array(data[i][0:2]))
 
-	return P
+        return P
 
 # ----------- Start
 
@@ -40,9 +40,9 @@ random.shuffle(data)
 
 pylab.hold(True)
 pylab.plot([p[0] for p in classA], [p[1] for p in classA],
-	'bo')
+        'bo')
 pylab.plot([p[0] for p in classB], [p[1] for p in classB],
-	'ro')
+        'ro')
 
 P = buildMatrix(data)
 q = matrix(numpy.array(len(data) * [-1]))
